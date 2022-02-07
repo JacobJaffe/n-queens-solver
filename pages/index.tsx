5bbd17e7901434df45168f297ff16f567aa7bd56
Jacob Jaffe <jacobmaxjaffe@gmail.com>
2022-02-07 13:31:31 -0800

    node types, cleanup on index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import { CodeBlock, Layouts } from "@template/components";
 
 const HomePage: React.FC = () => (
   <Layouts.Basic>
-    <p className="mt-3 text-lg text-gray-500">
+    <h1 className="mt-4 mb-4 text-2xl">Template Site</h1>
+    <p className="mt-3 mb-3 text-lg text-gray-500">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima sequi recusandae,
       porro maiores officia assumenda aliquam laborum ab aliquid veritatis impedit odit adipisci
       optio iste blanditiis facere. Totam, velit.
